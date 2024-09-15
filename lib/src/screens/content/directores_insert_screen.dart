129df8de9941c68a1a30aed773ec33df98957646
davidbarretolara <davidbarretolara@gmail.com>
2024-09-15 22:54:58 +0000

    Formualrio que pasa datos a variables - sin envio a servicio web aun

--- lib/src/screens/content/directores_insert_screen.dart
+++ lib/src/screens/content/directores_insert_screen.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/src/theme/app_theme.dart';
 
-class DirectoresInsertScreen extends StatelessWidget {
+class DirectoresInsertScreen extends StatefulWidget {
   const DirectoresInsertScreen({super.key});
+
+  @override
+  _DirectoresInsertScreenState createState() => _DirectoresInsertScreenState();
+}
+
+class _DirectoresInsertScreenState extends State<DirectoresInsertScreen> {
+  final TextEditingController nombresController = TextEditingController();
+  final TextEditingController peliculasController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: Text(
-            'Directores Insert',
-          ),
-        ),
+            child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: nombresController,
+                      decoration: const InputDecoration(
+                          labelText: 'Nombres', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: peliculasController,
+                      decoration: const InputDecoration(
+                          labelText: 'Peliculas', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                        onPressed: () {
+                          String nombres = nombresController.text;
+                          String peliculas = peliculasController.text;
+                        },
+                        child: const Text("Guardar"))
+                  ],
+                ))),
       ),
     );
   }
