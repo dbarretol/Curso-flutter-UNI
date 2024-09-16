162c1d9559e8a743d32c3c7367e70ccc0367bc8f
davidbarretolara <davidbarretolara@gmail.com>
2024-09-16 17:00:45 +0000

    Borrar datos de inicio de sesion

--- lib/src/screens/profile_screen.dart
+++ lib/src/screens/profile_screen.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/src/screens/splash_screen.dart';
 import 'package:myapp/src/theme/app_theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+
+  Future<void> _eliminarDato(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("datos");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const SplashScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: Text(
+            child: Column(children: [
+          const Text(
             'Profile screen',
           ),
-        ),
+          ElevatedButton(
+              onPressed: () {
+                _eliminarDato(context);
+              },
+              child: const Text("Cerrar sesión"))
+        ])),
       ),
     );
   }
