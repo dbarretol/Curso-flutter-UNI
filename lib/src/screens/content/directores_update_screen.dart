18386a47eef713a4195bb458b7117cff7840c6eb
davidbarretolara <davidbarretolara@gmail.com>
2024-09-17 05:42:00 +0000

    CAMBIO 08: Se agrega la funcionalidad de eliminar regsitros en DirectoresUpdateScreen

--- lib/src/screens/content/directores_update_screen.dart
+++ lib/src/screens/content/directores_update_screen.dart
@@ -3,11 +3,13 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:myapp/src/screens/content/directores_screen.dart';
 import 'package:myapp/src/theme/app_theme.dart';
 import 'package:http/http.dart' as http;
+import 'package:myapp/utils/app_colors.dart';
 
 class DirectoresUpdateScreen extends StatefulWidget {
   final String iddirector; //requiriendo parametro externo
   final String nombres;
   final String peliculas;
+
   const DirectoresUpdateScreen(
       {super.key,
       required this.iddirector,
@@ -46,12 +48,68 @@ class _DirectoresUpdateScreenState extends State<DirectoresUpdateScreen> {
         msg: "Se ha actualizado los datos del director de código $iddirector",
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.CENTER,
-        //timeInSecForIosWeb: 1,
         backgroundColor: Colors.red,
         textColor: Colors.white,
         fontSize: 16.0);
-    Navigator.push(context,
+    Navigator.pushReplacement(context,
         MaterialPageRoute(builder: (context) => const DirectoresScreen()));
+  }
+
+  Future<void> deleteDirector(String iddirector) async {
+    final response = await http.post(
+      Uri.parse('https://servicios.campus.pe/directoresdelete.php'),
+      body: {'iddirector': iddirector},
+    );
+
+    if (response.statusCode == 200) {
+      Fluttertoast.showToast(
+        msg: "El director ha sido eliminado exitosamente",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: AppColors.secondary.withOpacity(0.5),
+        textColor: Colors.black,
+        fontSize: 16.0,
+      );
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const DirectoresScreen()));
+    } else {
+      Fluttertoast.showToast(
+        msg: "Error al eliminar el director",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
+  }
+
+  void _confirmDelete(String iddirector) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Confirmar Eliminación"),
+          content:
+              const Text("¿Estás seguro de que deseas eliminar este director?"),
+          actions: [
+            TextButton(
+              child: const Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Cerrar el diálogo
+              },
+            ),
+            TextButton(
+              child: const Text("Sí"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Cerrar el diálogo
+                deleteDirector(iddirector); // Ejecutar la eliminación
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -59,41 +117,70 @@ class _DirectoresUpdateScreenState extends State<DirectoresUpdateScreen> {
     return MaterialApp(
       theme: AppTheme.lightTheme,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Actualizar Director'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
         body: Center(
-            child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      readOnly: true,
-                      controller: iddirectorController,
-                      decoration: const InputDecoration(
-                          labelText: 'Código', border: OutlineInputBorder()),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: nombresController,
-                      decoration: const InputDecoration(
-                          labelText: 'Nombres', border: OutlineInputBorder()),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: peliculasController,
-                      decoration: const InputDecoration(
-                          labelText: 'Peliculas', border: OutlineInputBorder()),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                        onPressed: () {
-                          String iddirector = iddirectorController.text;
-                          String nombres = nombresController.text;
-                          String peliculas = peliculasController.text;
-                          updateDirector(iddirector, nombres, peliculas);
-                        },
-                        child: const Text("Actualizar"))
-                  ],
-                ))),
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  readOnly: true,
+                  controller: iddirectorController,
+                  decoration: const InputDecoration(
+                    labelText: 'Código',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: nombresController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nombres',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: peliculasController,
+                  decoration: const InputDecoration(
+                    labelText: 'Películas',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    String iddirector = iddirectorController.text;
+                    String nombres = nombresController.text;
+                    String peliculas = peliculasController.text;
+                    updateDirector(iddirector, nombres, peliculas);
+                  },
+                  child: const Text("Actualizar"),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    size: 45.0,
+                  ),
+                  color: AppColors.primary,
+                  onPressed: () {
+                    _confirmDelete(
+                        widget.iddirector); // Confirmación de eliminación
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
