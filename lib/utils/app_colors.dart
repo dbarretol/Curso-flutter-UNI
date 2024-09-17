d3897957ddaaf0f02112f5fa6e3b937523889a80
davidbarretolara <davidbarretolara@gmail.com>
2024-09-17 00:32:26 +0000

    Cambio 01: nueva paleta de colores

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppColors {
 //Colores para fondos
 //Variant, son variantes mas oscuras
-  static const Color primary = Color(0xFFC7253E);
-  static const Color primaryVariant = Color(0xFF821131);
-  static const Color secondary = Color(0xFFFABC3F);
-  static const Color secondaryVariant = Color(0xFFE85C0D);
-  static const Color surface = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFF1F316F);
+  static const Color primaryVariant = Color(0xFF1A4870);
+  static const Color secondary = Color(0xFFFFAD60);
+  static const Color secondaryVariant = Color(0xFF5B99C2);
+  static const Color surface = Color(0xFFF9DBBA);
   static const Color error = Color(0xFFFF0000);
 
 //Colores para textos
